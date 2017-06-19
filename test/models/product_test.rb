21fe56e3d19b29c1273addbb6a551dd06a285503
Nurulazrad Bin Murad <nurulazradb@mykl-dts-mac07.hrbl.net>
2017-06-19 14:45:42 +0800

    Using fixture data for testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
 	test "product attributes must not be empty" do
 		product = Product.new
 		assert product.invalid?
@@ -42,4 +44,22 @@ class ProductTest < ActiveSupport::TestCase
 			assert new_product(name).valid?, "#{name} shouldn't be invalid"
 		end
 	end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'foo bar',
+                          price: 1,
+                          image_url: 'book.gif')
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'foo bar',
+                          price: 1,
+                          image_url: 'book.gif')
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 end
